2961fe7f28e0653ec96f288d7a83e6384c57f627
desguerra728 <desguerra728@phm.k12.in.us>
2026-04-24 09:49:18 -0400

    1

--- pokemonBattle.py
+++ pokemonBattle.py
@@ -6,19 +6,27 @@ root.title("Pokemon Battle")
 
 WIDTH = 1920
 HEIGHT = 1080
+img_dict = {}
+pokemon = []
 
 canvas = tk.Canvas(root, width = WIDTH, height = HEIGHT, bg = "white")
 canvas.pack()
 
-pika_img = "C:\Users\desguerra728\Documents\p7-final-project-desguerra728-dotcom\pika.png"
+def make_img(sprite_name, img_file, zoom):
+    img = tk.PhotoImage(file = img_file)
+    resized_img = img.subsample(zoom, zoom)
+    img_dict[sprite_name] = resized_img
 
-def make_img(image):
-    img = tk.PhotoImage(file = image)
-    return img
+def spawn_sprite(sprite_name, img_file, zoom, x, y):
+    make_img(sprite_name, img_file, zoom)
+    p = canvas.create_image(x, y, image = img_dict[sprite_name], anchor = "sw")
+    pokemon.append(p)
 
-def spawn(x, y, image):
-    
-    
+def gameloop():
+        
+    root.after(10, gameloop)
 
-
+spawn_sprite("pikachu", "spriteImages\pika.png", 1, -100, HEIGHT)
+spawn_sprite("pikachu2", "spriteImages\pikaFront.png", 2, 1200, 600)
+gameloop()
 root.mainloop()
